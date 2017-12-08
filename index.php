3ebd2cbcd657280d11ea4ee2f3c31a6c824984ea
“Kevin <logikevcoder@gmail.com>
2017-12-07 21:34:22 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Online Notes</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="initial-scale=1.0">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/app.css">
+		<link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+	</head>
+
+	<body>
+<!--============ NAVBAR =============-->
+<nav class="navbar navbar-custom navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand">Notes App</a>
+			<button type="button" class="navbar-toggle" data-target="#navCollapse" data-toggle="collapse">
+				<span class="sr-only">Toggle Navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+		<div class="navbar-collapse collapse" id="navCollapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="">Help</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Login</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+	
+	<div class="Jumbotron">
+		<h1>Online Notes App</h1>
+		<p>Take your notes wherever you go.</p>
+		<p>Easy to use and secure</p>
+		<button class="btn btn-lg signup">Sign up now</button>
+	</div>
+	
+	<footer>
+		<p>Created by Kevin Napier Copyright &copy; <?php $date = date("Y"); echo $date ?>.</p>
+	</footer>
+
+
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
