bc46011707c24d6aec0e33b5cbcedbfb9fa4235c
“Kevin <logikevcoder@gmail.com>
2017-12-11 08:47:52 -0600

    Added more php files, and comment structures

---
+++ signup.php
@@ -0,0 +1,18 @@
+<!--Start session-->
+<!--connect to the database-->
+
+
+<!--Check user inputs-->
+		<!--Define error messages-->
+		<!--Get username, email, password, password2-->
+		<!--Store errors in errors variable-->
+		<!--If there are any errors print error-->
+<!--No errors-->
+		<!--prepare variables for the queries-->
+		<!--If username exists in the user table print erro-->
+		<!--else-->
+				<!--If email exists in the users table print error-->
+				<!--else-->
+				<!--Create a unique activation code-->
+				<!--Insert user details and activation code in the users table-->
+			<!-- Send the user an email with a link to activate.php with their email and activation -->
